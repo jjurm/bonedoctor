1dd2952801a2eaed7a437eb9f44958cf9cd30e8b
jjurm <micko.juraj64@gmail.com>
2019-02-05 17:05:03 +0000

    Improve Main class

--- src/main/java/uk/ac/cam/cl/bravo/Main.kt
+++ src/main/java/uk/ac/cam/cl/bravo/Main.kt
@@ -1,9 +1,7 @@
 package uk.ac.cam.cl.bravo
 
 import uk.ac.cam.cl.bravo.classify.BodypartViewClassifier
-import uk.ac.cam.cl.bravo.dataset.Bodypart
 import uk.ac.cam.cl.bravo.dataset.Dataset
-import uk.ac.cam.cl.bravo.dataset.Normality
 import uk.ac.cam.cl.bravo.gui.DisplayImage
 import uk.ac.cam.cl.bravo.overlay.AffineTransformer
 import uk.ac.cam.cl.bravo.overlay.ImageOverlayImpl
@@ -16,6 +14,8 @@ import javax.imageio.ImageIO
 
 
 fun main(args: Array<String>) {
+    val dataset = Dataset()
+
     tryOverlay(
         "MURA-v1.1/train/XR_HAND/patient09734/study1_positive/image1_edit.png",
         "MURA-v1.1/train/XR_HAND/patient09734/study1_positive/image3_edit.png"
@@ -42,38 +42,41 @@ fun preprocessPipeline() {
     }
 }
 
-fun loadDataset() {
+/*fun loadDataset() {
     val dataset = Dataset()
     val subset = dataset.training.filter { it.normality == Normality.positive && it.bodypart == Bodypart.HAND }
-}
+}*/
 
 fun tryOverlay(file1: String, file2: String) {
-    println(file1)
-    println(file2)
-
     val base = ImageIO.read(File(file1))
     val sample = ImageIO.read(File(file2))
     DisplayImage(base)
     DisplayImage(sample)
 
-    val warper = InnerWarpTransformer(
-        parameterScale = 0.8,
-        parameterPenaltyScale = 1.0,
-        RESOLUTION = 4
-    )
+    lateinit var warper: InnerWarpTransformer
+
     val overlay = ImageOverlayImpl(
         arrayOf(
-            warper,
+            InnerWarpTransformer(
+                parameterScale = 0.8,
+                parameterPenaltyScale = 1.0,
+                RESOLUTION = 4
+            ).also { warper = it },
             AffineTransformer(parameterScale = 1.0, parameterPenaltyScale = 0.1)
         ),
         PixelSimilarity(parameterPenaltyWeight = 1.0, ignoreBorderWidth = 0.15),
         precision = 1e-4
     )
 
+    println("Fitting images...")
     val result = overlay.findBestOverlay(base, sample)
+
+    println("Generating overlay...")
     val parameters = result.point
     val transformed = overlay.applyTransformations(sample, parameters)
-    val overlayed = ImageTools.overlay(base, transformed)
-    warper.drawMarks(overlayed, parameters)
-    DisplayImage(overlayed)
+    val overlaid = ImageTools.overlay(base, transformed)
+    warper.drawMarks(overlaid, parameters)
+    DisplayImage(overlaid)
+
+    println("Done")
 }
