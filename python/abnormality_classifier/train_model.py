a00030108d70fe69a6e80366b871d6ac0c06ad2c
Leon Mlodzian <lam206@cam.ac.uk>
2019-02-11 08:22:07 +0000

    Added code for saving model after training and loading pretrained weights

--- python/abnormality_classifier/train_model.py
+++ python/abnormality_classifier/train_model.py
@@ -10,9 +10,10 @@ training_labels = numpy.load('training_labels')
 validation_images = numpy.load('validation_images')
 validation_labels = numpy.load('validation_labels')
 
-# build model
-print('building model')
+# build model and initialise with imagenet pretrained weights
+print('building model and initialising weights')
 densenet = DenseNet169(include_top=False, weights=None, input_tensor=None, input_shape=(320,320,3), pooling=None, classes=False)
+densenet.load_weights('densenet169_weights_tf_dim_ordering_tf_kernels_notop.h5')
 model = Sequential()
 model.add(densenet)
 model.add(Flatten())
@@ -22,6 +23,12 @@ model.add(Dense(1, activation='sigmoid'))
 print('compiling model')
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
+# set checkpoints
+print('setting checkpoints')
+filepath="weights-{epoch:02d}-{val_acc:.2f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=False)
+callbacks_list = [checkpoint]
+
 # fit model and evaluate on validation dataset
 print('fitting model')
-model.fit(x=training_images, y=training_labels, validation_data=(validation_images, validation_labels), epochs=2, batch_size=8)
+model.fit(x=training_images, y=training_labels, validation_data=(validation_images, validation_labels), callbacks=callbacks_list, epochs=2, batch_size=8)
