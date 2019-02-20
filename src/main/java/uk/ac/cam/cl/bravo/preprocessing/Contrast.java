eb8d05eeefe55f60711e6a302344f30f9e2a7980
Nicole Marielle Joseph <nicole@dhcp-10-248-97-241.eduroam.wireless.private.cam.ac.uk>
2019-02-20 15:13:12 +0000

    Border Detection and Removal

---
+++ src/main/java/uk/ac/cam/cl/bravo/preprocessing/Contrast.java
@@ -0,0 +1,127 @@
+package uk.ac.cam.cl.bravo.preprocessing;
+
+import javafx.util.Pair;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.TreeMap;
+
+public class Contrast {
+
+    public static BufferedImage contrast(BufferedImage srcFile, BufferedImage inputFile,
+                                         Pair<HashSet<Point2D>, HashSet<Point2D>> insideOutside) {
+
+        inputFile.getGraphics().drawImage(srcFile, 0, 0, null);
+
+        HashMap<Point2D, Integer> img = Statistics.imgMap(inputFile, EdgeRemoval.inside(insideOutside));
+
+        TreeMap<Integer, Integer> histo = Statistics.histogram(img);
+
+        int bT = blackThreshold(histo);
+        int wT = whiteThreshold(histo, img.size(), bT);
+
+        BufferedImage outputFile = new BufferedImage(inputFile.getWidth(), inputFile.getHeight(), inputFile.getType());
+
+        for (int x = 0; x < inputFile.getWidth(); x++) {
+            for (int y = 0; y < inputFile.getHeight(); y++) {
+                if (EdgeRemoval.outside(insideOutside).contains(new Point2D(x,y))){
+                    outputFile.setRGB(x, y, new Color(0,0,0).getRGB());
+                }
+                else {
+                    int rgba = inputFile.getRGB(x, y);
+                    Color col = new Color(rgba, true);
+                    int gs = (int) (0.3 * col.getRed() + 0.59 * col.getGreen() + 0.11 * col.getBlue());
+                    if (gs <= bT)
+                        col = new Color(0, 0, 0);
+                    else if (gs >= wT)
+                        col = new Color(255, 255, 255);
+                    else {
+                        int c = (int) Math.floor(255.0 / (wT - bT) * (gs - bT));
+                        col = new Color(c, c, c);
+                    }
+                    outputFile.setRGB(x, y, col.getRGB());
+                }
+            }
+        }
+
+        return outputFile;
+    }
+
+    public static BufferedImage contrast(BufferedImage srcFile,
+                                         Pair<HashSet<Point2D>, HashSet<Point2D>> insideOutside) {
+
+        HashMap<Point2D, Integer> img = Statistics.imgMap(srcFile, EdgeRemoval.inside(insideOutside));
+
+        TreeMap<Integer, Integer> histo = Statistics.histogram(img);
+
+        HashMap<Integer, Integer> histoSorted = Statistics.sortByValue(histo);
+
+        int bT = blackThreshold(histo);
+        int wT = whiteThreshold(histo, img.size(), bT);
+
+        BufferedImage outputFile = new BufferedImage(srcFile.getWidth(), srcFile.getHeight(), srcFile.getType());
+
+        for (int x = 0; x < srcFile.getWidth(); x++) {
+            for (int y = 0; y < srcFile.getHeight(); y++) {
+                if (EdgeRemoval.outside(insideOutside).contains(new Point2D(x,y))){
+                    outputFile.setRGB(x, y, new Color(0,0,0).getRGB());
+                }
+                else {
+                    int rgba = srcFile.getRGB(x, y);
+                    Color col = new Color(rgba, true);
+                    int gs = (int) (0.3 * col.getRed() + 0.59 * col.getGreen() + 0.11 * col.getBlue());
+                    if (gs <= bT)
+                        col = new Color(0, 0, 0);
+                    else if (gs >= wT)
+                        col = new Color(255, 255, 255);
+                    else {
+                        int c = (int) Math.floor(255.0 / (wT - bT) * (gs - bT));
+                        col = new Color(c, c, c);
+                    }
+                    outputFile.setRGB(x, y, col.getRGB());
+                }
+            }
+        }
+
+        return outputFile;
+    }
+
+    private static int blackThreshold(TreeMap<Integer, Integer> hist){
+
+        TreeMap<Integer, Integer> histo = hist;
+        for (int k: hist.keySet()){
+            if (hist.get(k) != 0)
+                histo.put(k, hist.get(k));
+        }
+        HashMap<Integer, Integer> diff = new HashMap<>();
+        for (int i=0; i<histo.size()-2; i++){
+            int a = (int) histo.keySet().toArray()[i];
+            int b = (int) histo.keySet().toArray()[i+1];
+            int d = Math.abs(histo.get(a) -
+                    histo.get(b));
+            diff.put(Math.max(a,b), d);
+        }
+        HashMap<Integer, Integer> sortDiff = Statistics.sortByValue(diff);
+        return ((Integer)sortDiff.keySet().toArray()[0]);
+    }
+
+    private static int whiteThreshold(TreeMap<Integer, Integer> histo, int size, int bT){
+
+        int sum=0;
+        for (int i=bT; i<=255; i++){
+            sum+=histo.get(histo.keySet().toArray()[i]);
+        }
+        int avg =  (int) (sum*1.0/(255-bT));
+        int w =255;
+        for (int i =250; i>=0; i--){
+            int x = histo.get(histo.keySet().toArray()[i]);
+            if (x>=avg) {
+                w = (Integer) histo.keySet().toArray()[i];
+                break;
+            }
+        }
+        return w;
+    }
+
+}
---
+++ src/main/java/uk/ac/cam/cl/bravo/preprocessing/Statistics.java
@@ -0,0 +1,96 @@
+package uk.ac.cam.cl.bravo.preprocessing;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.*;
+import java.util.List;
+
+public class Statistics {
+
+    public static TreeMap<Integer, Integer> histogram(HashMap<Point2D, Integer> img){
+
+        TreeMap<Integer, Integer> histo = new TreeMap<>();
+        for (Point2D p: img.keySet()) {
+            int c = (img.get(p)/5)*5;
+            if (histo.containsKey(c))
+                histo.put(c, histo.get(c) + 1);
+            else
+                histo.put(c, 1);
+        }
+        for (int i=0; i<256; i++){
+            if (!histo.containsKey(i))
+                histo.put(i, 0);
+        }
+        return histo;
+    }
+
+    public static HashMap<Integer, Integer> sortByValue(Map<Integer, Integer> tm)
+    {
+        List<Map.Entry<Integer, Integer> > list =
+                new LinkedList<Map.Entry<Integer, Integer> >(tm.entrySet());
+
+        Collections.sort(list, (o1, o2) -> -(o1.getValue()).compareTo(o2.getValue()));
+
+        HashMap<Integer, Integer> temp = new LinkedHashMap<>();
+        for (Map.Entry<Integer, Integer> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
+        }
+        return temp;
+    }
+
+    public static  HashMap<Point2D, Integer> imgMap(BufferedImage inputFile, HashSet<Point2D> inside){
+
+        HashMap<Point2D, Integer> img = new HashMap<>();
+
+        for (int x = 0; x <inputFile.getWidth(); x++) {
+            for (int y = 0; y < inputFile.getHeight(); y++) {
+                if (inside.contains(new Point2D(x,y))) {
+                    int rgba = inputFile.getRGB(x, y);
+                    Color col = new Color(rgba, true);
+                    img.put(new Point2D(x, y), (int) (0.3 * col.getRed() + 0.59 * col.getGreen()
+                            + 0.11 * col.getBlue()));
+                }
+            }
+        }
+        return img;
+    }
+
+    public static  HashMap<Point2D, Integer> imgMap(BufferedImage inputFile){
+
+        HashMap<Point2D, Integer> img = new HashMap<>();
+
+        for (int x = 0; x <inputFile.getWidth(); x++) {
+            for (int y = 0; y < inputFile.getHeight(); y++) {
+                int rgba = inputFile.getRGB(x, y);
+                Color col = new Color(rgba, true);
+                img.put(new Point2D(x, y), (int) (0.3 * col.getRed() + 0.59 * col.getGreen() + 0.11 * col.getBlue()));
+            }
+        }
+        return img;
+    }
+
+    public static double var(ArrayList<Double> vals){
+        int mean=0;
+        for (double d: vals)
+            mean+=d;
+        mean = mean/vals.size();
+        int var = 0;
+        for (double d:vals)
+            var+=(d-mean)*(d-mean);
+        var = var/vals.size();
+        return var;
+    }
+
+    public static int  getGrayScale(int rgb) {
+        int r = (rgb >> 16) & 0xff;
+        int g = (rgb >> 8) & 0xff;
+        int b = (rgb) & 0xff;
+
+        //from https://en.wikipedia.org/wiki/Grayscale, calculating luminance
+        int gray = (int)(0.2126 * r + 0.7152 * g + 0.0722 * b);
+        //int gray = (r + g + b) / 3;
+
+        return gray;
+    }
+}
+
