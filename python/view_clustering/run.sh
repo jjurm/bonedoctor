2290d6b93abc40db8ac5a1726fcc5efba62a74a0
kwotsin <leeks_50996@hotmail.com>
2019-02-21 02:25:28 +0000

    added script for inference to all bodyparts

---
+++ python/view_clustering/run.sh
@@ -0,0 +1,11 @@
+"""
+Performs inference on all bodyparts of the MURA dataset and collect the results.
+"""
+
+python3 main.py --body_part=XR_SHOULDER
+python3 main.py --body_part=XR_FOREARM
+python3 main.py --body_part=XR_WRIST
+python3 main.py --body_part=XR_ELBOW
+python3 main.py --body_part=XR_FINGER
+python3 main.py --body_part=XR_HAND
+python3 main.py --body_part=XR_HUMERUS
