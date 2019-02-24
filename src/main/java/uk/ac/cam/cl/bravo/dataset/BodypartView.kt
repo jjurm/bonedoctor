cd1017b44b78d184e006af674585903ecdddb771
jjurm <micko.juraj64@gmail.com>
2019-02-24 22:44:28 +0000

    Implement BodypartView.equals

--- src/main/java/uk/ac/cam/cl/bravo/dataset/BodypartView.kt
+++ src/main/java/uk/ac/cam/cl/bravo/dataset/BodypartView.kt
@@ -1,4 +1,4 @@
 package uk.ac.cam.cl.bravo.dataset
 
 /** Each Bodypart can have various views, identified by an index within the body part. */
-class BodypartView(val bodypart: Bodypart, val value: Int)
+data class BodypartView(val bodypart: Bodypart, val value: Int)
