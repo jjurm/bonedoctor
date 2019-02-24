2949520916eb44b1ae80104c4db4b1a2e3c70f5d
jjurm <micko.juraj64@gmail.com>
2019-02-24 23:36:14 +0000

    Load BodypartViews to ImageSamples

--- src/main/java/uk/ac/cam/cl/bravo/dataset/Dataset.kt
+++ src/main/java/uk/ac/cam/cl/bravo/dataset/Dataset.kt
@@ -1,5 +1,6 @@
 package uk.ac.cam.cl.bravo.dataset
 
+import uk.ac.cam.cl.bravo.classify.BodypartViewClassifierImpl
 import java.io.BufferedReader
 import java.io.FileReader
 import java.io.IOException
@@ -18,9 +19,10 @@ class Dataset @Throws(IOException::class) constructor() {
         const val DIR_PREPROCESSED = "MURA-preprocessed/"
         private const val CSV_TRAIN_IMAGE_PATHS = DIR + "train_image_paths.csv"
         private const val CSV_VALID_IMAGE_PATHS = DIR + "valid_image_paths.csv"
+        private const val VIEW_CLUSTERING_OUTPUT_DIR = "python/view_clustering/output/"
 
     }
-    
+
     val training: Map<String, ImageSample>
     val validation: Map<String, ImageSample>
 
@@ -41,7 +43,7 @@ class Dataset @Throws(IOException::class) constructor() {
     private fun loadImageSamples(csvFilename: String): Map<String, ImageSample> {
         return loadPaths(csvFilename).mapNotNull { path ->
             try {
-                path to ImageSample(path)
+                path to ImageSample(path, getBodypartViewOf(path))
             } catch (e: IllegalArgumentException) {
                 System.err.println(e.message)
                 null
@@ -49,12 +51,24 @@ class Dataset @Throws(IOException::class) constructor() {
         }.toMap()
     }
 
+    private val bodypartViews: Map<String, BodypartView>
+
     private fun getBodypartViewOf(path: String): BodypartView {
-        TODO()
+        // This throws an exception if the path is not found in the map
+        try {
+            val key = path.removePrefix("MURA-v1.1/").substringBeforeLast('_') + "/" + path.substringAfterLast('_')
+            return bodypartViews.getValue(key)
+        } catch (e: NoSuchElementException) {
+            throw RuntimeException("The image $path has no BodypartView assigned in $VIEW_CLUSTERING_OUTPUT_DIR")
+        }
     }
 
     init {
         checkDatasetFolderExists()
+
+        bodypartViews = BodypartViewClassifierImpl.buildBodyPartViews(VIEW_CLUSTERING_OUTPUT_DIR)
+
+        // load list of all images
         training = loadImageSamples(CSV_TRAIN_IMAGE_PATHS)
         validation = loadImageSamples(CSV_VALID_IMAGE_PATHS)
         println("${training.size} training samples loaded.")
