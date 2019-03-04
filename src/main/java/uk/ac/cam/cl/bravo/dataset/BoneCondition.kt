2eb17b2ac2390129225af658f3636a6337637cf9
Leon Mlodzian <lam206@cam.ac.uk>
2019-03-04 09:59:33 +0000

    jj bju

--- src/main/java/uk/ac/cam/cl/bravo/dataset/BoneCondition.kt
+++ src/main/java/uk/ac/cam/cl/bravo/dataset/BoneCondition.kt
@@ -1,7 +1,7 @@
 package uk.ac.cam.cl.bravo.dataset
 
 enum class BoneCondition(val label: String) {
-    NORMAL("negative"), ABNORMAL("positive");
+    NORMAL("normal"), ABNORMAL("abnormal");
 
     companion object {
         private val map: Map<String, BoneCondition> = values().map { it.label to it }.toMap()
